e0f7dda9b68e48b0d0ec739db77805ca70024a77
Jingying Gong <jingying.gong@sc.com>
2019-12-25 13:05:11 +0800

    [Logan] config with postgres

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
   secreteKey: "gjy",
-  postgresql: "postgres://postgres:123qwe@localhost:5432/postgres"
+  postgresql: "postgres://postgres:123qwe@localhost:5432/postgres",
+  //postgresql: "postgres://postgres:123qwe@localhost:5432/postgres"
 };
 export default CONFIG;
